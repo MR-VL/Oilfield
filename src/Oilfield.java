5e318edd11ada99250470893391474d236278c7f
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-22 16:21:59 -0500

    base input

--- src/Oilfield.java
+++ src/Oilfield.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class Oilfield
 {
     public static void main(String[] args) {
 
+
+        int spaces = getInput("Enter the total number of spaces you want on the grid: ", "Spaces");
+
+        int height = getInput("Enter the height you want the grid to be: ", "Height");
+
+        if (height > spaces) throw heightError(){
+            System.out.println("Error height cannot be greater than total spaces.");
+        }
+
+        System.out.println(spaces + " " + height);
+
+        //CreateField field = new CreateField(spaces, height);
+        //field.create();
+
+    }
+
+    public static int getInput(String prompt, String identifier){
+        Scanner input = new Scanner(System.in);
+        System.out.println(prompt);
+        int num = input.nextInt();
+
+        return validate(num, identifier);
+    }
+
+    public static int validate(int number, String type){
+
+        while(number <=0){
+            System.out.printf("Error, %s must be a number greater than 0.", type);
+            String prompt = "Enter number of " + type;
+            number = getInput(prompt, type);
+        }
+        return number;
     }
 }
