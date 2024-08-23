347955c9c7834ca0b78b5ff2a26f40c220ab7b1c
MR-VL <107425742+MR-VL@users.noreply.github.com>
2024-08-23 15:25:16 -0500

    Clear console works on cmd prmpt but not in intellij.
    Intellij has no native support for cls
    
    leave as is

--- src/Formatting/ClearConsole.java
+++ src/Formatting/ClearConsole.java
@@ -3,23 +3,8 @@ package Formatting;
 import Exception.ConsoleClsError;
 public class ClearConsole {
     public ClearConsole() throws ConsoleClsError {
-        try
-        {
-            final String os = System.getProperty("os.name");
-
-            if (os.contains("Windows"))
-            {
-                Runtime.getRuntime().exec("cls");
-            }
-            else
-            {
-                Runtime.getRuntime().exec("clear");
-            }
-        }
-        catch (final Exception e)
-        {
-           throw new ConsoleClsError("CRITICAL ERROR while attempting to clear console.");
-        }
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 
 }
